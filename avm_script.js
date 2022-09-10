cb14a1e43b79db09cdbd16d369f21d07c26d7a1b
Alph4rd <15257735+Alph4rd@users.noreply.github.com>
2022-09-10 18:56:55 -0300

    Search for packet methods instead of relying on id

--- avm_script.js
+++ avm_script.js
@@ -1,5 +1,3 @@
-const packet_sender_id  = 27119;
-const packet_handler_id = 27126;
 var patterns = { 
     darkbot : "ff ff 01 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00"
 };
@@ -502,6 +500,7 @@ findPattern(patterns.darkbot, function(addr, size) {
     avm.toplevel        = vtable.add(0x8).readPointer();
     var vtable_init     = vtable.add(0x10).readPointer();
     var vtable_scope    = vtable_init.add(0x18).readPointer();
+
     avm.abc_env         = vtable_scope.add(0x10).readPointer();
     avm.core            = traits.add(0x8).readPointer();
     avm.constant_pool   = avm.abc_env.add(0x8).readPointer();
@@ -510,9 +509,19 @@ findPattern(patterns.darkbot, function(addr, size) {
     var ns_list          = avm.core.add(offsets.ns_list).readPointer();
     var ns_count         = avm.core.add(0x80).readPointer();
 
-    // ids are not reliable, might change after an update
-    packet_handler = method_list.add(0x10 + packet_handler_id * 8).readPointer();
-    packet_sender  = method_list.add(0x10 + packet_sender_id  * 8).readPointer();
+    var conn_manager    = main_address.add(0x230).readPointer();
+
+    var conn = conn_manager.add(0xd8).readPointer();
+
+    packet_sender = getObjectMethods(conn)[13].add(0x10).readPointer();
+    var packet_handler_class = findClassClosure(closure => {
+        var methods = getObjectMethods(closure);
+        if (methods.length == 8 && !methods[7].equals(0) && getMethodName(methods[7].add(0x10).readPointer()) == "execute")
+        {
+            packet_handler = methods[7].add(0x10).readPointer();
+        }
+    });
+
 
     // Iterate namespaces
     for (var i = 0, c = 0; i < 0x40000 && c < ns_count; i++) {
