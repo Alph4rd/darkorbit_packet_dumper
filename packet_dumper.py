7538514c3d3dc5cb46b552175b27fdde09d02c95
Alph4rd <15257735+Alph4rd@users.noreply.github.com>
2022-09-11 14:05:35 -0300

    Python changes
    Some cleanup
    
    Add stacktrace to output
    
    Process selection

--- packet_dumper.py
+++ packet_dumper.py
@@ -3,15 +3,84 @@ import psutil
 import frida
 import json
 import sys
-import os
-import re
 from datetime import datetime
 
-blacklist = []
-output_file = None
-output_json = False
+SCRIPT_PATH = "avm_script.js"
+
+# Packet id blacklist
+BLACKLIST = []
+
 #IN_PACKET = 0
 #OUT_PACKET = 1
+
+class PacketDumper:
+    def __init__(self, pid : int, out_file_name = None, json_only = False):
+        if out_file_name:
+            self.output_file = open(out_file_name, "r")
+        else:
+            self.output_file = None
+        self.json_only = json_only
+        self.pid = pid
+
+    def __del__(self):
+        if self.output_file:
+            self.output_file.close()
+
+    def write_output(self, packet_type, packet_id, name, payload, trace):
+        output = ""
+        current_t = datetime.now().isoformat()
+
+        if self.json_only:
+            output = json.dumps({ "type" : packet_type, "time" : current_t, "id": packet_id, "name" : name, "data" : payload, "stacktrace" : trace })
+        else:
+            json_content = json.dumps(payload, indent=4)
+            if packet_type == 0:
+                output = "Received packet {0: >5s} [{1: >6d}] [{2:s}]\n{3:s}\n".format(name, packet_id, current_t, json_content)
+            else:
+                output = "Sending packet  {0: >5s} [{1: >6d}] [{2:s}]\n{3:s}\n".format(name, packet_id, current_t, json_content)
+                output += f"Stacktrace for [{packet_id}]\n"
+                output += "\n".join(["{0:3d} - {1:s}".format(i, name) for i,name in enumerate(trace)])
+
+        output += "\n"
+
+        if self.output_file:
+            self.output_file.write(output)
+
+        print(output)
+
+    def on_message(self, msg, data):
+        if msg["type"] == "send":
+            payload = msg["payload"]
+            try:
+                BLACKLIST.index(int(payload["id"]))
+                return
+            except:
+                pass
+
+            self.write_output(payload["type"], int(payload["id"]), payload["name"], payload["packet"], payload["stacktrace"])
+
+        elif msg["type"] == "error":
+            print(json.dumps(msg, indent=4))
+        else:
+            print(msg)
+
+    def run(self):
+        try:
+            session = frida.attach(self.pid)
+        except frida.ProcessNotRespondingError as e:
+            print(e)
+            print("Try launching the chromium process with --no-sandbox")
+            sys.exit(-1)
+
+        script = session.create_script(open(SCRIPT_PATH).read())
+
+        script.on('message', self.on_message)
+
+        script.load()
+
+        input('[!] Press <Enter> to stop.')
+
+        session.detach()
 
 def find_flash_process():
     for proc in psutil.process_iter(["pid", "ppid", "cmdline"]):
@@ -19,85 +88,31 @@ def find_flash_process():
             flash_lib = next(filter(lambda m: m.path.find("Flash.ocx") >= 0, proc.memory_maps()))
         except:
             flash_lib = None
+
         if flash_lib != None or (proc.info["cmdline"] and "--type=ppapi" in proc.info["cmdline"]):
-            return(proc.info["pid"])
-    return None
-
-def write_output(packet_type, packet_id, name, payload):
-    global output_json, output_file
-
-    output = ""
-    current_t = datetime.now().isoformat()
-
-    if output_json:
-        output = json.dumps({ "type" : packet_type, "time" : current_t, "id": packet_id, "name" : name, "data" : payload })
-    else:
-        json_content = json.dumps(payload, indent=4)
-        if packet_type == 0:
-            output = "Received packet {0: >5s} [{1: >6d}] [{2:s}]\n{3:s}".format(name, packet_id, current_t, json_content)
-        else:
-            output = "Sending packet  {0: >5s} [{1: >6d}] [{2:s}]\n{3:s}".format(name, packet_id, current_t, json_content)
-
-    if output_file:
-        output_file.write(output+ "\n")
-
-    print(output)
-
-def on_message(msg, data):
-    if msg["type"] == "send":
-        payload = msg["payload"]
-        try:
-            blacklist.index(int(payload["id"]))
-            return
-        except:
-            pass
-        write_output(payload["type"], int(payload["id"]), payload["name"], payload["packet"])
-
-    elif msg["type"] == "error":
-        print(json.dumps(msg, indent=4))
-    else:
-        print(msg)
+            return proc.info.get("pid")
+    return 0
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--log', dest="log_file", nargs="?", const=(datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")), type=str, help="Output file")
+    parser.add_argument('-l', '--log',  dest="log_file", nargs="?", const=(datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")), type=str, help="Output file")
     parser.add_argument('-j', '--json', dest="json", action="store_true", required=False, help="Json ouput.")
-
+    parser.add_argument('-p', '--pid',  dest="pid", type=int, required=False, help="Process id")
     return parser.parse_args()
 
 def main():
-    global output_file, output_json
     args = parse_args()
 
-    if args.log_file != None:
-        print(args.log_file)
-        output_file = open(args.log_file, "w")
-    output_json = args.json == True
-
-    pid = find_flash_process()
+    pid = args.pid or find_flash_process()
 
     if not pid:
-        print("[!] Failed to find process.")
+        print("[!] No pid. Stopping.")
         return
+    else:
+        print(f"[+] Dumping on process {pid}.")
 
-    print(f"[+] Found process{pid}.")
-
-    try:
-        session = frida.attach(pid)
-    except frida.ProcessNotRespondingError as e:
-        print(e)
-        print("Try launching the chromium process with --no-sandbox")
-        sys.exit(-1)
-
-    script = session.create_script(open("avm_script.js").read())
-
-    script.on('message', on_message)
-
-    script.load()
-
-    input('[!] Press <Enter> to stop.')
-
-    session.detach()
+    dumper = PacketDumper(pid, args.log_file, args.json)
+    dumper.run()
 
 if __name__ == "__main__":
     main()
